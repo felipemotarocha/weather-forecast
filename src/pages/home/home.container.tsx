b3e81463765b8b8ee00a36bf5171ff6f9fe16e29
Felipe Rocha <felipemota.rocha@gmail.com>
2020-11-11 18:43:06 -0300

    feat: fetching current forecast data

--- src/pages/home/home.container.tsx
+++ src/pages/home/home.container.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-import { Location } from '../../types/forecast-data.types';
+import { Current, Location } from '../../types/forecast-data.types';
 
 import HomePage from './home.page';
 
@@ -10,12 +10,19 @@ export interface HomeContainerProps {}
 
 const HomeContainer: React.FunctionComponent<HomeContainerProps> = () => {
 	const [location, setLocation] = useState<Location | null>(null);
+	const [current, setCurrent] = useState<Current | null>(null);
 
 	useEffect(() => {
 		const fetchForecastData = async () => {
 			const {
 				data: {
 					location: { country, localtime, name, region },
+					current: {
+						condition: { text },
+						temp_c,
+						wind_mph,
+						humidity,
+					},
 				},
 			} = await axios.get(
 				`https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=São Paulo&days=7`
@@ -27,13 +34,22 @@ const HomeContainer: React.FunctionComponent<HomeContainerProps> = () => {
 				name,
 				region,
 			};
+
+			const current: Current = {
+				condition: text,
+				temp_c,
+				wind_mph,
+				humidity,
+			};
+
 			setLocation(location);
+			setCurrent(current);
 		};
 
 		fetchForecastData();
 	}, []);
 
-	return <HomePage location={location} />;
+	return <HomePage location={location} current={current} />;
 };
 
 export default HomeContainer;
--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import { Container } from './home.styles';
-import { Location } from '../../types/forecast-data.types';
+import { Current, Location } from '../../types/forecast-data.types';
 
 import CurrentDayForecast from '../../components/current-day-forecast/current-day-forecast.component';
 
 interface HomePageProps {
 	location: Location | null;
+	current: Current | null;
 }
 
-const HomePage: React.FunctionComponent<HomePageProps> = ({ location }) => {
+const HomePage: React.FunctionComponent<HomePageProps> = () => {
 	return (
 		<Container>
 			<CurrentDayForecast />
